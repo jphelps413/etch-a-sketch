27c5af474e6b1df4efc4e91b603c3ba81ed5d7c4
John Phelps <john@fractograf.com>
2016-11-30 18:08:46 -0500

    First working gridbag layout

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,4 +1,28 @@
+gRows = 16
+gCols = 16
+
+function testCSS() {
+  console.log("testCSS")
+  $('div').css({
+          'background-color': 'green',
+            'height':           '250px',
+            'width':            '350px',
+  })
+}
+
 $(document).ready( function() {
-  $('div').css('background-color','green')
-  //console.log("HERE")
+  console.log("buildGrid: Grid will be "+ gRows +" by "+ gCols)
+
+  for( r = 0; r < gRows; r++ ) {
+    rowId = "r"+r
+    rowDiv = '<div class="row" id="'+rowId+'"></div>'
+    console.log( "Append="+rowDiv )
+    $('#gridbag').append(rowDiv)
+    for( c = 0; c < gCols; c++ ) {
+      cellId = "c"+r+"."+c
+      cellDiv = '<div class="cell" id="'+cellId+'"></div>'
+      console.log("Append "+rowDiv+"="+cellDiv)
+      $('#'+rowId).append(cellDiv)
+    }
+  }
 })
